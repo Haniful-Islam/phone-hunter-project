678ce3fb3195de85faeab17ad8af2a5aa4fe4059
Haniful-Islam <haniful360@gmail.com>
2022-03-01 18:50:35 +0600

    added sliec

--- js/mobile.js
+++ js/mobile.js
@@ -1,20 +1,29 @@
 const searchPhone = () => {
     const searchText = document.getElementById('input-field');
     const searchValue = searchText.value;
-    // console.log(searchValue);
+
     // clear data
     searchText.value = '';
-
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`
-    fetch(url)
-        .then(response => response.json())
-        .then(data => displayPhoneResult(data.data))
+    // errorMessage
+    if (searchValue == '') {
+        const errorMessage = document.getElementById('erro-message');
+        errorMessage.style.color = "red";
+        errorMessage.innerText = 'Please write some to display!!!'
+    }
+    else {
+        // load data
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`
+        fetch(url)
+            .then(response => response.json())
+            .then(data => displayPhoneResult(data.data))
+    }
 }
+
 const displayPhoneResult = phones => {
     // console.log(phones)
     const searchResult = document.getElementById('search-result')
     searchResult.textContent = "";
-    for (const phone of phones) {
+    for (const phone of phones.slice(0, 20)) {
         // console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
@@ -23,10 +32,10 @@ const displayPhoneResult = phones => {
             <div class="card h-100 p-4">
                 <img  src="${phone.image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                <h3 class="card-title">${phone.brand.slice(0, 20)}</h3>
-                <h4 class="card-title">${phone.phone_name.slice(0, 20)}</h4>
+                <h3 class="card-title">${phone.brand}</h3>
+                <h4 class="card-title">${phone.phone_name}</h4>
                 </div>
-                <a  onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-outline-info">Details</a>
+                <a  onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-outline-info">See Details</a>
             </div>
         `;
         searchResult.appendChild(div);
@@ -44,31 +53,29 @@ const loadPhoneDetails = phoneId => {
 const singleDisplayDetails = phone => {
     console.log(phone);
     const phoneDetails = document.getElementById('phone-details');
-
     const div = document.createElement('div');
     div.classList.add('card');
-
     div.innerHTML = `
         <div class="card px-5 pt-3">
             <img  src="${phone.image}" class=" card-img-top" alt="...">
-            <div style="text-align:center" class="card-body">
+            <div class="card-body">
                 <h3 class="card-title">${phone.brand}</h3>
                 <h4 class="card-title">${phone.name}</h4 >
-                <h4>MainFeatures:</h4>
-                <p>ReleaseDate: ${phone.releaseDate}</p>                
-                <p>Storage: ${phone.mainFeatures.storage}</p> 
-                <p>DisplaySize: ${phone.mainFeatures.displaySize}</p>                
-                <p>Chipset: ${phone.mainFeatures.chipSet}</p>  
-                <p>Sensors: ${phone.mainFeatures.sensors}</p>  
-                <h4>OthersFeatures:</h4>  
-                <p>Chipset: ${phone.others.WLAN}</p>                           
-                <p>Chipset: ${phone.others.Bluetooth}</p>                           
-                <p>Chipset: ${phone.others.GPS}</p>                           
+                <h4 class="text-info">MainFeatures:</h4>
+                <p><span class="fw-bold">ReleaseDate:</span> ${phone.releaseDate}</p>                
+                <p><span class="fw-bold">Storage:</span> ${phone.mainFeatures.storage}</p> 
+                <p><span class="fw-bold">DisplaySize:</span> ${phone.mainFeatures.displaySize}</p>                
+                <p><span class="fw-bold">Chipset:</span> ${phone.mainFeatures.chipSet}</p>  
+                <p> <span class="fw-bold">Sensors:</span> ${phone.mainFeatures.sensors}</p>  
+
+                <h4 class="text-info">OthersFeatures:</h4>  
+                <p><span class="fw-bold">WLAN:</span> ${phone.others.WLAN}</p>                           
+                <p><span class="fw-bold">Bluetooth:</span> ${phone.others.Bluetooth}</p>                           
+                   
+                <p><span class="fw-bold">GPS:</span>${phone.others.GPS}</p>                        
             </div >
         </div >
-         
+
     `;
     phoneDetails.appendChild(div);
-
-
 }
